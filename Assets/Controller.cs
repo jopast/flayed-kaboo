3337b31ae6c1ae7adcddf2110835cf818bd792d7
TheDogPotato <JohnAylwyn@outlook.com>
2026-06-13 10:58:50 -0400

    fueiw

--- Assets/Controller.cs
+++ Assets/Controller.cs
@@ -5,6 +5,7 @@ public class Controller : MonoBehaviour
     public float speed = 10.0f;
     public float crawlX;
     public float crawlY;
+    public Camera Cam;
     private Rigidbody rb;
     private void Start()
     {
@@ -25,10 +26,15 @@ public class Controller : MonoBehaviour
 
     void FixedUpdate()
     {
-        Vector3 movement = new Vector3(crawlX, 0.0f, crawlY);
-        rb.AddForce(movement * speed);
-
         
+        Vector3 rtransform = Cam.transform.right;
+        Vector3 rtransforward=Cam.transform.forward;
+        rtransform.y=0;
+        rtransforward.y = 0;
+        rtransform.Normalize();
+        rtransforward.Normalize();
+        Vector3 movement =(rtransforward*crawlY+rtransform*crawlX);
+        rb.AddForce(movement * speed, ForceMode.Force);
 
     }
 
